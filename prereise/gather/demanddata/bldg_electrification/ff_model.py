cf829245301bae7fafe5a228a7c59bdd3c7f85ee
Daniel Olsen <daniel@breakthroughenergy.org>
2021-08-11 10:06:41 -0700

    refactor: simplify monthly average calculations

--- prereise/gather/demanddata/bldg_electrification/ff_model.py
+++ prereise/gather/demanddata/bldg_electrification/ff_model.py
@@ -10,12 +10,15 @@ from prereise.gather.demanddata.bldg_electrification import const
 
 def calculate_state_slopes(puma_data, year):
     dti = pd.date_range(start=f"{year}-01-01", end=f"{year}-12-31 23:00:00", freq="H")
+    hours_in_month = dti.month.value_counts()
 
     # Load in historical 2010 fossil fuel usage data
     data_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), "data")
     ng_usage_data = {
-        "res": pd.read_csv(os.path.join(data_dir, "ng_monthly_mmbtu_2010_res.csv"),
-        "com": pd.read_csv(os.path.join(data_dir, "ng_monthly_mmbtu_2010_com.csv"),
+        clas: pd.read_csv(
+            os.path.join(data_dir, f"ng_monthly_mmbtu_2010_{clas}.csv"), index_col=0
+        )
+        for clas in {"res", "com"}
     }
     fok_usage_data = pd.read_csv(
         os.path.join(data_dir, "fok_data_bystate_2010.csv"), index_col="state"
@@ -92,6 +95,8 @@ def calculate_state_slopes(puma_data, year):
             totfuel = fok + other + natgas.sum()
             # Scale total fossil fuel usage by monthly natural gas
             ff_usage_data_it = totfuel * natgas / natgas.sum()
+            # Fossil fuel average monthly mmbtu, normalized by hours in month
+            ff_monthly_it = ff_usage_data_it / hours_in_month
 
             # Hourly heating degrees for all pumas in a given state, multiplied by their corresponding area and percent fossil fuel, summed up to one hourly list
             hd_hourly_it_sh = (
@@ -101,6 +106,7 @@ def calculate_state_slopes(puma_data, year):
                 .mul(areas_ff_sh_it, axis=0)
                 .sum(axis=0)
             )
+            hd_monthly_it_sh = hd_hourly_it_sh.groupby(dti.month).mean()
 
             if clas == "res":
                 hd_hourly_it_dhw = (
@@ -108,31 +114,7 @@ def calculate_state_slopes(puma_data, year):
                     .mul(areas_ff_dhw_it, axis=0)
                     .sum(axis=0)
                 )
-
-                # Average hd per month
-                df_hourly_it = pd.DataFrame(
-                    {
-                        "month": dti.month,
-                        "hd_hourly_it_sh": hd_hourly_it_sh,
-                        "hd_hourly_it_dhw": hd_hourly_it_dhw,
-                    }
-                )
-                hd_group_sh = df_hourly_it["hd_hourly_it_sh"].groupby(df_hourly_it["month"])
-                hd_group_dhw = df_hourly_it["hd_hourly_it_dhw"].groupby(
-                    df_hourly_it["month"]
-                )
-                df_monthly_it = pd.DataFrame(
-                    {
-                        "hd_avg_sh": hd_group_sh.mean(),
-                        "hd_avg_dhw": hd_group_dhw.mean(),
-                        "n_hrs": hd_group_sh.count(),
-                    }
-                )
-                # Fossil fuel average monthly mmbtu, normalized by hours in month
-                df_monthly_it["ff_monthly_mmbtu"] = [
-                    ff_usage_data_it[i] / list(df_monthly_it["n_hrs"])[i]
-                    for i in range(len(df_monthly_it))
-                ]
+                hd_monthly_it_dhw = hd_hourly_it_dhw.groupby(dti.month).mean()
 
                 # Fitting function: Returns difference between fitted equation and actual fossil fuel usage for the least_squares function to minimize
                 def func_r(par, sh, dhw, ff):
@@ -143,16 +125,11 @@ def calculate_state_slopes(puma_data, year):
                     )
                     return err
 
-                # Input data points for fitting
-                data_sh = np.array(df_monthly_it["hd_avg_sh"])
-                data_dhw = np.array(df_monthly_it["hd_avg_dhw"])
-                data_ff = df_monthly_it["ff_monthly_mmbtu"]
-
                 # Least squares solver
                 lm_it = least_squares(
                     func_r,
                     const.bounds_lower_res,
-                    args=(data_sh, data_dhw, data_ff),
+                    args=(hd_monthly_it_sh, hd_monthly_it_dhw, ff_monthly_it),
                     bounds=(const.bounds_lower_res, const.bounds_upper_res),
                 )
 
@@ -166,9 +143,9 @@ def calculate_state_slopes(puma_data, year):
                 residuals = list(lm_it.fun)
                 sumres = 0
                 sumtot = 0
-                for i in range(len(list(data_ff))):
+                for i in range(len(list(ff_monthly_it))):
                     sumres += residuals[i] ** 2
-                    sumtot += (list(data_ff)[i] - np.mean(data_ff)) ** 2
+                    sumtot += (list(ff_monthly_it)[i] - np.mean(ff_monthly_it)) ** 2
                 r2 = 1 - (sumres / sumtot)
 
                 # Add coefficients to output dataframe
@@ -181,9 +158,7 @@ def calculate_state_slopes(puma_data, year):
                     par_dhw_l,
                     par_other_c,
                 ]
-
             else:
-
                 hd_hourly_it_other = (
                     temps_pumas_transpose.applymap(
                         lambda x: max(x - const.temp_ref[clas], 0)
@@ -191,31 +166,7 @@ def calculate_state_slopes(puma_data, year):
                     .mul(areas_ff_other_it, axis=0)
                     .sum(axis=0)
                 )
-
-                # Average hd per month
-                df_hourly_it = pd.DataFrame(
-                    {
-                        "month": dti.month,
-                        "hd_hourly_it_sh": hd_hourly_it_sh,
-                        "hd_hourly_it_other": hd_hourly_it_other,
-                    }
-                )
-                hd_group_sh = df_hourly_it["hd_hourly_it_sh"].groupby(df_hourly_it["month"])
-                hd_group_other = df_hourly_it["hd_hourly_it_other"].groupby(
-                    df_hourly_it["month"]
-                )
-                df_monthly_it = pd.DataFrame(
-                    {
-                        "hd_avg_sh": hd_group_sh.mean(),
-                        "hd_avg_other": hd_group_other.mean(),
-                        "n_hrs": hd_group_sh.count(),
-                    }
-                )
-                # Fossil fuel average monthly mmbtu, normalized by hours in month
-                df_monthly_it["ff_monthly_mmbtu"] = [
-                    ff_usage_data_it[i] / list(df_monthly_it["n_hrs"])[i]
-                    for i in range(len(df_monthly_it))
-                ]
+                hd_monthly_it_other = hd_hourly_it_other.groupby(dti.month).mean()
 
                 # Fitting function: Returns difference between fitted equation and actual fossil fuel usage for the least_squares function to minimize
                 def func_c(par, sh, other, ff):
@@ -227,16 +178,11 @@ def calculate_state_slopes(puma_data, year):
                     )
                     return err
 
-                # Input data points for fitting
-                data_sh = np.array(df_monthly_it["hd_avg_sh"])
-                data_other = np.array(df_monthly_it["hd_avg_other"])
-                data_ff = df_monthly_it["ff_monthly_mmbtu"]
-
                 # Least squares solver
                 lm_it = least_squares(
                     func_c,
                     const.bounds_lower_com,
-                    args=(data_sh, data_other, data_ff),
+                    args=(hd_monthly_it_sh, hd_monthly_it_other, ff_monthly_it),
                     bounds=(const.bounds_lower_com, const.bounds_upper_com),
                 )
 
@@ -251,9 +197,9 @@ def calculate_state_slopes(puma_data, year):
                 residuals = list(lm_it.fun)
                 sumres = 0
                 sumtot = 0
-                for i in range(len(list(data_ff))):
+                for i in range(len(list(ff_monthly_it))):
                     sumres += residuals[i] ** 2
-                    sumtot += (list(data_ff)[i] - np.mean(data_ff)) ** 2
+                    sumtot += (list(ff_monthly_it)[i] - np.mean(ff_monthly_it)) ** 2
                 r2 = 1 - (sumres / sumtot)
 
                 # Add coefficients to output dataframe
